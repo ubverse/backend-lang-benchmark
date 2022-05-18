d087337ea552b00f34e96b833c426a3721cd5297
Caian Ertl <hi@caian.org>
2022-05-18 02:29:00 -0300

    feat(in-memory): (WIP) initial kotlin project with springboot

---
+++ 01-in-memory/kotlin/src/main/kotlin/com/upsetbit/kotapi/inmemory/InmemoryApplication.kt
@@ -0,0 +1,11 @@
+package com.upsetbit.kotapi.inmemory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InmemoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<InmemoryApplication>(*args)
+}
---
+++ 01-in-memory/kotlin/src/test/kotlin/com/upsetbit/kotapi/inmemory/InmemoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.upsetbit.kotapi.inmemory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InmemoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
